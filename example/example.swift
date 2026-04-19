9ac0adb15bd6ddaab700e52416fafd039c27ab5a
Matthew Nitschke <matthewcnitschke@gmail.com>
2026-04-19 15:42:57 -0600

    added additional fields to example

--- example/example.swift
+++ example/example.swift
@@ -5,6 +5,8 @@ let serviceUUID = CBUUID(string: "6a521c59-55b5-4384-85c0-6534e63fb09e")
 let setPointUUID = CBUUID(string: "6a521c60-55b5-4384-85c0-6534e63fb09e")
 let boilerCurrentUUID = CBUUID(string: "6a521c61-55b5-4384-85c0-6534e63fb09e")
 let boilerTargetUUID = CBUUID(string: "6a521c66-55b5-4384-85c0-6534e63fb09e")
+let groupheadCurrentUUID = CBUUID(string: "6a521c62-55b5-4384-85c0-6534e63fb09e")
+let fluidLevelUUID = CBUUID(string: "6a521c63-55b5-4384-85c0-6534e63fb09e")
 
 let app = NSApplication.shared
 let delegate = AppDelegate()
@@ -20,7 +22,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var boilerCurrent: Double = 0
     var boilerTarget: Double = 0
     var setPoint: Double = 0
-    var waterStatus: String = "OK"
+    var groupheadCurrent: Double = 0
+    var fluidLevel: UInt32 = 0
 
     func applicationDidFinishLaunching(_ notification: Notification) {                        
       centralManager = CBCentralManager(delegate: self, queue: .main)
@@ -28,9 +31,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func updateOutput() {
       updateMultilineOutput(lines: [
-        "- Set Point: \(setPoint)",
-        "- Boiler Current: \(boilerCurrent)",
-        "- Boiler Target: \(boilerTarget)"
+        "  * Set Point: \(setPoint)",
+        "  * Boiler Current: \(boilerCurrent)",
+        "  * Boiler Target: \(boilerTarget)",
+        "  * Grouphead Current: \(groupheadCurrent)",
+        "  * Fluid Level: \(fluidLevel == 0 ? "OK" : "Needs Water")"
       ])
     }
 
@@ -88,7 +93,7 @@ extension AppDelegate: CBPeripheralDelegate {
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
         guard let services = peripheral.services else { return }
         for service in services {
-            peripheral.discoverCharacteristics([setPointUUID, boilerCurrentUUID, boilerTargetUUID], for: service)
+            peripheral.discoverCharacteristics([setPointUUID, boilerCurrentUUID, boilerTargetUUID, groupheadCurrentUUID, fluidLevelUUID], for: service)
         }
     }
     
@@ -100,22 +105,35 @@ extension AppDelegate: CBPeripheralDelegate {
     }
     
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-        guard let data = characteristic.value, data.count >= 8 else { return }
+        guard let data = characteristic.value, data.count >= 4 else { return }
         
-        var temp = data.withUnsafeBytes { $0.load(as: Double.self) }
-        temp = (temp * 100).rounded() / 100
-        
-        switch characteristic.uuid {
-        case setPointUUID:
-          setPoint = temp
-        case boilerCurrentUUID:
-          boilerCurrent = temp
-        case boilerTargetUUID:
-          boilerTarget = temp
-        default:
-          break
+        if data.count >= 8 {
+          var temp = data.withUnsafeBytes { $0.load(as: Double.self) }
+          temp = (temp * 100).rounded() / 100
+          
+          switch characteristic.uuid {
+          case setPointUUID:
+            setPoint = temp
+          case boilerCurrentUUID:
+            boilerCurrent = temp
+          case boilerTargetUUID:
+            boilerTarget = temp
+          case groupheadCurrentUUID:
+            groupheadCurrent = temp
+          default:
+            break
+          }
+        } else if data.count == 4 {
+          let value = data.withUnsafeBytes { $0.load(as: UInt32.self) }
+          
+          switch characteristic.uuid {
+          case fluidLevelUUID:
+            fluidLevel = value
+          default:
+            break
+          }
         }
-
+        
         updateOutput()
     }
 }
